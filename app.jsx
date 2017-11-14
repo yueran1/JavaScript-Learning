ccbaf49a018337554727a03abe27f8a2da1504bb
yueran1 <yueran1@ualberta.ca>
2017-11-14 15:27:28 -0800

    make the code dry, do not repeat your code

--- app.jsx
+++ app.jsx
@@ -2,23 +2,25 @@ var main=function(){
     "use strict"
     window.alert("Fuck you! Haha")
 
-    //In this function, We listening on the Click(button)
-    
-    
-    $(".comment-input button").on("click", function (event){
-      
+    var addComment=function(){
         if ($(".comment-input input").val() !=""){
             var $new_comment= $("<p>");
             var $text_comment = $(".comment-input input").val();
             $new_comment.text($text_comment);
-    
+            $new_comment.hide();
             $(".comments").append($new_comment);
+            $new_comment.fadeIn();
             $(".comment-input input").val("");
 
         } else{
             window.alert("Fuck you, dont leave empty comment!")
         }
-       
+
+    }
+
+    //In this function, We listening on the Click(button)  
+    $(".comment-input button").on("click", function (event){
+        addComment();
 
     });
     
@@ -27,23 +29,8 @@ var main=function(){
     $(".comment-input input").on("keypress", function (event){
 
         if (event.keyCode===13){
-            if ($(".comment-input input").val() !=""){
-                var $new_comment= $("<p>");
-                var $text_comment = $(".comment-input input").val();
-                $new_comment.text($text_comment);
-                //Jquery fadein function, before fadein, you should use hide() function
-                $new_comment.hide();
-        
-                $(".comments").append($new_comment);
-                $new_comment.fadeIn();
-                $(".comment-input input").val("");
-    
-            } else{
-                window.alert("Fuck you, dont leave empty comment!");
-            }
-
+            addComment();
         }
-
 
     });
 };
