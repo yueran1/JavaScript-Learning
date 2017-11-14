01f1fb6f9925b933b763aa545ab754b4616d4981
yueran1 <yueran1@ualberta.ca>
2017-11-14 15:19:45 -0800

    add jquery fadein property

--- app.jsx
+++ app.jsx
@@ -4,6 +4,7 @@ var main=function(){
 
     //In this function, We listening on the Click(button)
     
+    
     $(".comment-input button").on("click", function (event){
       
         if ($(".comment-input input").val() !=""){
@@ -30,12 +31,15 @@ var main=function(){
                 var $new_comment= $("<p>");
                 var $text_comment = $(".comment-input input").val();
                 $new_comment.text($text_comment);
+                //Jquery fadein function, before fadein, you should use hide() function
+                $new_comment.hide();
         
                 $(".comments").append($new_comment);
+                $new_comment.fadeIn();
                 $(".comment-input input").val("");
     
             } else{
-                window.alert("Fuck you, dont leave empty comment!")
+                window.alert("Fuck you, dont leave empty comment!");
             }
 
         }
