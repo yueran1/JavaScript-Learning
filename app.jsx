f1396d2eebc108da26b7d06e7330bc029dada964
yueran1 <yueran1@ualberta.ca>
2017-11-13 21:56:59 -0800

    upload javascript example

---
+++ app.jsx
@@ -0,0 +1,13 @@
+var main=function(){
+    "use strict"
+    window.alert("Fuck you! Haha")
+
+    $(".comment-input button").on("click", function (event){
+        console.log("hello world!");
+        window.alert("fuck you again!!");
+
+    });
+};
+
+$(document).ready(main);
+
