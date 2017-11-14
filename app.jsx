7ba4092b85024c9d05c018fb172ba1693d86e470
yueran1 <yueran1@ualberta.ca>
2017-11-14 15:08:41 -0800

    Except click button to submit, allow enter key to submit

--- app.jsx
+++ app.jsx
@@ -2,14 +2,44 @@ var main=function(){
     "use strict"
     window.alert("Fuck you! Haha")
 
+    //In this function, We listening on the Click(button)
+    
     $(".comment-input button").on("click", function (event){
-        //console.log("hello world!");
-        //window.alert("fuck you again!!");
-        var $new_comment= $("<p>");
-        var $text_comment = $(".comment-input input").val();
-        $new_comment.text($text_comment);
+      
+        if ($(".comment-input input").val() !=""){
+            var $new_comment= $("<p>");
+            var $text_comment = $(".comment-input input").val();
+            $new_comment.text($text_comment);
+    
+            $(".comments").append($new_comment);
+            $(".comment-input input").val("");
 
-        $(".comments").append($new_comment);
+        } else{
+            window.alert("Fuck you, dont leave empty comment!")
+        }
+       
+
+    });
+    
+
+    //In this function we listening on the keypress(key board enter--"input")
+    $(".comment-input input").on("keypress", function (event){
+
+        if (event.keyCode===13){
+            if ($(".comment-input input").val() !=""){
+                var $new_comment= $("<p>");
+                var $text_comment = $(".comment-input input").val();
+                $new_comment.text($text_comment);
+        
+                $(".comments").append($new_comment);
+                $(".comment-input input").val("");
+    
+            } else{
+                window.alert("Fuck you, dont leave empty comment!")
+            }
+
+        }
+
 
     });
 };
